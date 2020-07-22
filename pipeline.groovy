25eb020ae227b26137257261ef9a11d4ba647f8b
twayn <Twayn@ya.ru>
2020-07-22 14:30:15 +0300

    Pipeline deploy and smoke

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,14 +2,18 @@ pipeline {
     agent any
 
     stages {
-        stage('Build') {
+        stage('Checkout from SCM') {
             steps {
-                echo 'Build..'
+                echo 'Checkout..'
                 git branch: 'master',
                         credentialsId: 'b42b8605-6d70-4b2d-8101-ace7e3362763',
                         url: 'https://github.com/Twayn/JenkinsDemo.git'
-
-                bat "./mvnw package"
+            }
+        }
+        stage('Build') {
+            steps {
+                echo 'Build..'
+                bat "./mvnw package -Dmaven.test.skip=true"
             }
         }
         stage('Test') {
@@ -21,6 +25,15 @@ pipeline {
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
+                bat "java -jar ./target/demo-0.0.1-SNAPSHOT.war"
+            }
+        }
+        stage('Smoke') {
+            steps {
+                echo 'Smoke....'
+
+                def status = bat(script: "curl http://localhost:8081/hello", returnStdout: true)
+                echo "$status"
             }
         }
     }
